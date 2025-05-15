a2ff115fc0cd9a7569ba33e252372a0da59dbcaa
kimyt990501 <kimyt990501@naver.com>
2025-05-15 17:32:13 +0900

    feat : 이벤트 목록 조회 및 상세 조회 api 라우팅 추가

--- gateway-svc/src/proxy/proxy.controller.ts
+++ gateway-svc/src/proxy/proxy.controller.ts
@@ -33,6 +33,30 @@ export class ProxyController {
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN, Role.OPERATOR)
+  @Get('/events')
+  async getEvents(@Req() req, @Res() res) {
+    const { data } = await firstValueFrom(
+      this.httpService.get('http://event-svc:3200/events/summary', {
+        headers: { Authorization: req.headers.authorization },
+      }),
+    );
+    return res.send(data);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN, Role.OPERATOR)
+  @Get('/events/:id')
+  async getEventDetail(@Req() req, @Param('id') id: string, @Res() res) {
+    const { data } = await firstValueFrom(
+      this.httpService.get(`http://event-svc:3200/events/detail/${id}`, {
+        headers: { Authorization: req.headers.authorization },
+      }),
+    );
+    return res.send(data);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.USER, Role.ADMIN)
   @Post('/requests/:eventId')
   async requestReward(@Req() req, @Param('eventId') id: string, @Res() res) {
