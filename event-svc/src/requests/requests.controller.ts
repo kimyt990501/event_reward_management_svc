0336ad578d3f0a98f7e15285ca5b37272325b55a
kimyt990501 <kimyt990501@naver.com>
2025-05-16 14:14:38 +0900

    log : request reward 관련 로그 추가

--- event-svc/src/requests/requests.controller.ts
+++ event-svc/src/requests/requests.controller.ts
@@ -5,22 +5,31 @@ import {
   Get,
   Req,
   UseGuards,
+  Inject
 } from '@nestjs/common';
 import { RequestsService } from './requests.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { RolesGuard } from '../auth/roles.guard';
 import { Roles } from '../auth/roles.decorator';
 import { Role } from '../common/roles.enum';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 
 @Controller('requests')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class RequestsController {
-  constructor(private readonly service: RequestsService) {}
+  constructor(private readonly service: RequestsService,
+    @Inject(WINSTON_MODULE_NEST_PROVIDER)
+    private readonly logger: Logger,
+  ) {}
 
   @Post(':eventId')
   @Roles(Role.USER)
   async request(@Param('eventId') id: string, @Req() req) {
-    return this.service.requestReward(req.user.userId, id);
+    this.logger.log(`[RequestsController] User ${req.user?.userId} is requesting reward for event ${id}`, 'info');
+    this.logger.log(`req.user: ${JSON.stringify(req.user)}`, 'debug');
+    this.logger.log(`eventId param: ${id}`, 'debug');
+    return this.service.requestReward(req.user?.userId, id);
   }
 
   @Get('my')
