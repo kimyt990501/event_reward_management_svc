5656386cd785aff45004150dde1e79d5e8e05569
kimyt990501 <kimyt990501@naver.com>
2025-05-16 16:47:45 +0900

    feat :  response 인터셉터 및 http 예외 처리 핉터 적용

--- gateway-svc/src/main.ts
+++ gateway-svc/src/main.ts
@@ -6,13 +6,21 @@ import { winstonLoggerConfig } from './logger/logger.config';
 import { Logger } from 'winston';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ResponseFormatInterceptor } from './common/interceptors/response-format.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: WinstonModule.createLogger(winstonLoggerConfig),
   });
   const logger = app.get<Logger>(WINSTON_MODULE_NEST_PROVIDER);
-  app.useGlobalInterceptors(new LoggingInterceptor(logger));
+
+  const loggingInterceptor = new LoggingInterceptor(logger);
+  const responseFormatInterceptor = app.get(ResponseFormatInterceptor);
+
+  app.useGlobalInterceptors(loggingInterceptor, responseFormatInterceptor);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  
   const config = app.get(ConfigService);
   await app.listen(config.get('GATEWAY_PORT') || 3300);
 }
