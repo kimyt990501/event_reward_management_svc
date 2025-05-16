e7217eeac283a9c1c95791b6079f55c6ada6c3bf
kimyt990501 <kimyt990501@naver.com>
2025-05-16 14:13:54 +0900

    log : 로그 추가

--- event-svc/src/auth/jwt.strategy.ts
+++ event-svc/src/auth/jwt.strategy.ts
@@ -1,17 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject} from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(configService: ConfigService,
+    @Inject(WINSTON_MODULE_NEST_PROVIDER)
+    private readonly logger: Logger,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'secret',
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
+
+    this.logger.log('[JwtStrategy] JWT strategy initialized', 'info');
   }
 
   async validate(payload: any) {
+    this.logger.log(`[JwtStrategy] JWT validated. Payload: ${JSON.stringify(payload)}`, 'debug');
+
     return {
       userId: payload.sub,
       email: payload.email,
