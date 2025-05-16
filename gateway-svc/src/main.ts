94812177474fde1f09ee115b2ccb460a767365de
kimyt990501 <kimyt990501@naver.com>
2025-05-16 14:53:46 +0900

    feat : logging interceptor 모듈 추가

--- gateway-svc/src/main.ts
+++ gateway-svc/src/main.ts
@@ -3,11 +3,16 @@ import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { WinstonModule } from 'nest-winston';
 import { winstonLoggerConfig } from './logger/logger.config';
+import { Logger } from 'winston';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: WinstonModule.createLogger(winstonLoggerConfig),
   });
+  const logger = app.get<Logger>(WINSTON_MODULE_NEST_PROVIDER);
+  app.useGlobalInterceptors(new LoggingInterceptor(logger));
   const config = app.get(ConfigService);
   await app.listen(config.get('PORT') || 3300);
 }
