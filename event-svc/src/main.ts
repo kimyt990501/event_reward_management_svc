f8e4c418ea3061eb9c42216a056ea3b1db04c346
kimyt990501 <kimyt990501@naver.com>
2025-05-16 14:13:41 +0900

    feat : winston 로거 모듈 추가

--- event-svc/src/main.ts
+++ event-svc/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { WinstonModule } from 'nest-winston';
+import { winstonLoggerConfig } from './logger/logger.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger(winstonLoggerConfig),
+  });
   const config = app.get(ConfigService);
   const port = config.get<number>('PORT') || 3200;
   await app.listen(port);
