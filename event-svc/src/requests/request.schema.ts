74c7c48023cb3bf6b7d1e5cad4cf87612721787e
kimyt990501 <50610894+kimyt990501@users.noreply.github.com>
2025-05-18 19:11:08 +0900

    refactor : request 스키마 필드 수정

--- event-svc/src/requests/request.schema.ts
+++ event-svc/src/requests/request.schema.ts
@@ -4,10 +4,10 @@ import { Document, Types } from 'mongoose';
 @Schema()
 export class Request extends Document {
   @Prop({ required: true })
-  userId: string;
+  user_email: string;
 
   @Prop({ type: Types.ObjectId, ref: 'Event', required: true })
-  eventId: Types.ObjectId;
+  event_title: Types.ObjectId;
 
   @Prop({ default: 'PENDING' })
   status: 'PENDING' | 'SUCCESS' | 'FAILED';
