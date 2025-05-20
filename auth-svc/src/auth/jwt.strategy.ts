0bfb778dc1926d95be2a70149466343324bf6f6d
kimyt990501 <kimyt990501@naver.com>
2025-05-20 09:11:12 +0900

    refactor : jwt secret 키 환경변수에서 로드하게 수정

--- auth-svc/src/auth/jwt.strategy.ts
+++ auth-svc/src/auth/jwt.strategy.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'secret', // 🔐 실제 배포 시 env로 관리
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
